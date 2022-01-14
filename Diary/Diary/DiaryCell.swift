b7a19338694cf195ff810a43de199a03bc1c95f6
YuSeongChoi <marine8743@naver.com>
2022-01-14 19:15:05 +0900

    ADD 일기 앱 기본 UI 구성

---
+++ Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by Mac on 2022/01/14.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by Mac on 2022/01/14.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
